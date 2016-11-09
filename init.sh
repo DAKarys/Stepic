945f7f79148fb9a4cde48a5b9174485c0a6bf0ea
DAKarys <alun.karys@yandex.ru>
2016-11-09 13:18:57 +0300

    First

---
+++ init.sh
@@ -0,0 +1,2 @@
+sudo ln etc/nginx.conf /etc/nginx/sites-enable/default
+sudo /etc/init.d/nginx restart
---
+++ start.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/DAKarys/Stepic.git
+git config --global user.name "DAKarys"
+git config --global user.email "alun.karys@yandex.ru"
+
